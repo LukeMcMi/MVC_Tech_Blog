2fa2c38def6fbe07509cd4d6a6215d4794735cdb
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2021-11-28 19:31:00 +1100

    Updated dashboard route to include comments. Additional typo updated.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,12 +1,31 @@
 const router = require("express").Router();
-const { Post } = require("../models");
+const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", withAuth, (req, res) => {
     Post.findAll({
       where: {
         userId: req.session.userId
-      }
+      },
+      attributes: [
+        'id',
+        'title',
+        'content',
+        'created_at'
+    ],
+    include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
     })
       .then(dbPostData => {
         const posts = dbPostData.map((post) => post.get({ plain: true }));
