b00e5c9ba6591fdc35f68fb8a960abf80a11a4b3
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2021-12-01 11:24:13 +1100

    Updated models, simplified.

--- models/Comment.js
+++ models/Comment.js
@@ -5,36 +5,13 @@ class Comment extends Model {}
 
 Comment.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-  },
-  comment_text: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-          model: 'user',
-          key: 'id'
-      }
-  },
-  post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-          model: 'post',
-          key: 'id'
-      }
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   },
   {
     sequelize,
-    freezeTableName: true,
-    underscored: true,
     modelName: 'comment'
   }
 );
