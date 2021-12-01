7e011d2ac4b8fff9677eb992cd4796388c1a8bf2
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2021-12-01 11:16:37 +1100

    updated dashboard and home route

--- controllers/index.js
+++ controllers/index.js
@@ -7,8 +7,5 @@ const apiRoutes = require("./api")
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
-router.use((req, res) => {
-    res.status(404).end();
-});
 
 module.exports = router;
